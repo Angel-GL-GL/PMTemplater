edda01fde52900d11c7ae5dc9078bd99fd3b59a5
Angel-GL-GL <angelel.garciaia@gmail.com>
2025-01-04 03:20:09 -0600

    feat(entity)
    create UsersEntity class

---
+++ src/main/java/com/github/angelglgl/pmtemplater/persistence/entities/UsersEntity.java
@@ -0,0 +1,24 @@
+package com.github.angelglgl.pmtemplater.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class UsersEntity {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "user_firstname")
+    private String userFirstname;
+    @Column(name = "user_lastname")
+    private String userLastname;
+    @Column(name = "user_role")
+    private String userRole;
+    private String userEmail;
+    private String userPassword;
+}
