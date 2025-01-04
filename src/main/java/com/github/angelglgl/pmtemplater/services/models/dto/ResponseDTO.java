13af5dfc7919472099fd3caf892a99fcba40a454
Angel-GL-GL <angelel.garciaia@gmail.com>
2025-01-04 03:22:17 -0600

    feat(dto)
    create ResponseDTO class

---
+++ src/main/java/com/github/angelglgl/pmtemplater/services/models/dto/ResponseDTO.java
@@ -0,0 +1,11 @@
+package com.github.angelglgl.pmtemplater.services.models.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDTO {
+    private int numOfErrors;
+    private String message;
+}
