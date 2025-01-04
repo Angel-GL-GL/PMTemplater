f659f6013747793687496ab461e8cc84ea0ac54a
Angel-GL-GL <angelel.garciaia@gmail.com>
2025-01-04 03:22:49 -0600

    feat(dto)
    create LoginDTO class

---
+++ src/main/java/com/github/angelglgl/pmtemplater/services/models/dto/LoginDTO.java
@@ -0,0 +1,11 @@
+package com.github.angelglgl.pmtemplater.services.models.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+    private String userEmail;
+    private String userPassword;
+}
