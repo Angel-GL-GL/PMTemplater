6db1ea12c81cbd0f593aae858c249bee4aa01bb4
Angel-GL-GL <angelel.garciaia@gmail.com>
2025-01-03 22:53:55 -0600

    feat(project)
    create Spring Boot project

---
+++ src/main/java/com/github/angelglgl/pmtemplater/PmtemplaterApplication.java
@@ -0,0 +1,13 @@
+package com.github.angelglgl.pmtemplater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtemplaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtemplaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/angelglgl/pmtemplater/PmtemplaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.angelglgl.pmtemplater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtemplaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
