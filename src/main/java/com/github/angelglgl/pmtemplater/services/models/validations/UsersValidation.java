e92dd441c68a10b880f3aa9d5d059609ecb01cbc
Angel-GL-GL <angelel.garciaia@gmail.com>
2025-01-08 15:01:10 -0600

    feat(validation)
    add validation for userTeam in UsersValidation

--- src/main/java/com/github/angelglgl/pmtemplater/services/models/validations/UsersValidation.java
+++ src/main/java/com/github/angelglgl/pmtemplater/services/models/validations/UsersValidation.java
@@ -23,6 +23,13 @@ public class UsersValidation {
             response.setMessage("Lastname is required, must be between 3 and 30 characters");
         }
 
+        if(user.getUserTeam() == null || 
+            user.getUserTeam().length() < 2 ||
+            user.getUserTeam().length() > 30){
+            response.setNumOfErrors(response.getNumOfErrors() + 1);
+            response.setMessage("Team is required, must be between 3 and 20 characters");
+        }
+
         if(user.getUserRole() == null || 
             user.getUserRole().length() < 5 ||
             user.getUserRole().length() > 20){
